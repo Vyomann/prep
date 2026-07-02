ef0c93b15e893035360ad84183fa9aa8394adb6d
Vyomann <85079083+Vyomann@users.noreply.github.com>
2026-07-02 23:39:16 +0530

    D10

--- Q38_Pyramid_Star.cpp
+++ Q38_Pyramid_Star.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main(){
     int n,i,j;
-    cout<<"Enter the number : ";
+    cout<<"Enter the nuumber : ";
     cin>>n;
-    for(i=n;i>=1;i--){
-        for(j=i;j>=1;j--){
+    for(i=1;i<=n;i++){
+        for(j=1; j<=n-i;j++){
+            cout<<" ";
+        }
+        for(j=1;j<=2*i-1;j++){
             cout<<"*";
         }
         cout<<endl;
