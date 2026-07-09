61443e669c0aa8eaddcbbf6c5c5293121b33ff76
Vyomann <85079083+Vyomann@users.noreply.github.com>
2026-07-09 22:45:22 +0530

    D12

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,18 +1,27 @@
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int n, i, j, num = 1;
-//     cout << "Enter the number : ";
-//     cin >> n;
+int main(){
+    int n, i, j;
+    cout << "Enter the number : ";
+    cin >> n;
 
-//     for(i = 1; i <= n; i++){
-//         for(j = 1; j <= i; j++){
-//             cout << num << " ";
-//             num++;
-//         }
-//         cout << endl;
-//     }
-//     return 0;
-// }
+    // Upper Half
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= i; j++)         cout << "*";              // left stars
+        for(j = 1; j <= 2*(n-i); j++)   cout << " ";              // middle spaces
+        for(j = 1; j <= i; j++)         cout << "*";              // right stars
+        cout << endl;
+    }
+
+    // Lower Half
+    for(i = n; i >= 1; i--){
+        for(j = 1; j <= i; j++)         cout << "*";              // left stars
+        for(j = 1; j <= 2*(n-i); j++)   cout << " ";              // middle spaces
+        for(j = 1; j <= i; j++)         cout << "*";              // right stars
+        cout << endl;
+    }
+
+    return 0;
+}
