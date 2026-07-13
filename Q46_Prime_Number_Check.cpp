f6ee34051473d83fc4b49dc541d1d2e418511542
Vyomann <85079083+Vyomann@users.noreply.github.com>
2026-07-14 03:19:25 +0530

    D15

---
+++ Q46_Prime_Number_Check.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    if (n <= 1) {
+        cout<<"Not Prime";
+    }
+    else {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            cout << "Prime";
+        }
+        else {
+            cout << "Not Prime";
+        }
+    }
+    return 0;
+}
